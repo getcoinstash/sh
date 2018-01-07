6478c38d716a04bacaef8e71f6c84213cbc82798
getcoinstash <34668601+getcoinstash@users.noreply.github.com>
2018-01-07 05:24:14 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #! /bin/bash
 sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+apt update
+git clone https://github.com/fireice-uk/xmr-stak.git
+mkdir xmr-stak/build
+    cd xmr-stak/build
+    cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
