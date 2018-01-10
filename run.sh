6129c8985efa0e96e733957a74ce81a0e0bd751f
getcoinstash <34668601+getcoinstash@users.noreply.github.com>
2018-01-10 07:56:13 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo git clone https://github.com/fireice-uk/xmr-stak.git
 mkdir xmr-stak/build
     cd xmr-stak/build
-    cmake3 .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
+    cmake .. -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF
 make install | bash
 cd ~/sh/xmr-stak/build/bin
 cp ~/sh/input.txt ~/sh/xmr-stak/build/bin/input.txt
